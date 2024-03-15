47684fb518c1b8cb316002def70e386e1c636517
maratabdulin <abdulinmm@gmail.com>
2024-03-15 10:51:40 +0000

    Auto-generated build for DEV branch

--- 282.js
+++ 282.js
@@ -1240,7 +1240,7 @@ let NewPostComponent = /*#__PURE__*/(() => {
       this.newPostForm = this.fb.group({
         title: new _angular_forms__WEBPACK_IMPORTED_MODULE_13__.FormControl('', [_angular_forms__WEBPACK_IMPORTED_MODULE_13__.Validators.required]),
         description: new _angular_forms__WEBPACK_IMPORTED_MODULE_13__.FormControl('', [_angular_forms__WEBPACK_IMPORTED_MODULE_13__.Validators.required, _angular_forms__WEBPACK_IMPORTED_MODULE_13__.Validators.maxLength(this.maxLengthDescription)]),
-        tags: new _angular_forms__WEBPACK_IMPORTED_MODULE_13__.FormControl('', [_utils_tags_validator__WEBPACK_IMPORTED_MODULE_5__.TagsValidator.pattern(/^[a-zA-Z0-9]+$/)]),
+        tags: new _angular_forms__WEBPACK_IMPORTED_MODULE_13__.FormControl('', [_utils_tags_validator__WEBPACK_IMPORTED_MODULE_5__.TagsValidator.pattern(/^[a-zA-Z0-9 ]+$/)]),
         agreeToTerms: new _angular_forms__WEBPACK_IMPORTED_MODULE_13__.FormControl(false, [_angular_forms__WEBPACK_IMPORTED_MODULE_13__.Validators.requiredTrue])
       });
       this.URLS = _enums_urls__WEBPACK_IMPORTED_MODULE_6__.URLS;
@@ -1336,18 +1336,22 @@ let NewPostComponent = /*#__PURE__*/(() => {
     onKeyDown(event) {
       const tagsControl = this.newPostForm.get('tags');
       if ((event.key === ' ' || event.key === 'Enter') && !(tagsControl && tagsControl?.hasError('pattern'))) {
-        const inputValue = event.target.value.trim();
-        if (inputValue !== '') {
-          const existingTag = this.tagList.find(t => t.name === inputValue);
-          const newTag = existingTag ? existingTag : {
-            name: inputValue,
-            id: ''
-          };
-          this.store.dispatch((0,_store_newPost_newPost_actions__WEBPACK_IMPORTED_MODULE_1__.addTagAction)({
-            newTag
-          }));
+        const inputValue = event.target.value.trim().split(/\s+/).filter(tag => tag);
+        if (inputValue.length > 0 && inputValue.length <= 50) {
+          for (const tag of inputValue) {
+            const existingTag = this.tagList.find(t => t.name === tag);
+            const newTag = existingTag ? existingTag : {
+              name: tag,
+              id: ''
+            };
+            this.store.dispatch((0,_store_newPost_newPost_actions__WEBPACK_IMPORTED_MODULE_1__.addTagAction)({
+              newTag
+            }));
+          }
           this.filteredTagList = [];
           event.target.value = '';
+        } else if (inputValue.length > 50) {
+          this.showErrorMessage(`Apologies, you've exceeded the maximum allowable tag count of 50. Now it's ${inputValue.length}`);
         }
         event.preventDefault();
       }
